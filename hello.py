fefcc32563e3809518dacdcada8f5c1ccb61d7c3
Kino <zhouhuang@gmail.com>
2016-01-06 13:52:22 +0800

    Call person instead
    We should call person instead of global Hello

--- hello.py
+++ hello.py
@@ -3,8 +3,13 @@
 def Hello(name):
    print("Hello, {0}!".format(name))
 
-Hello("Zhou")
-
 class Person():
-    def __init__(name):
+    def __init__(self, name):
         self.name = name
+
+    def hello(self, name="Kino"):
+    	print("{0} says hello to {1}!".format(self.name, name))
+
+zhou = Person("Zhou")
+Hello(zhou.name)
+zhou.hello()
